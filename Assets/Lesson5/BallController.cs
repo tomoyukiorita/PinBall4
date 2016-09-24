f8047ab319bb5a7bc9d76fbf2cb2b3ca4bd899e4
折田知之 <orita.tomoyuki@gmail.com>
2016-09-24 19:40:47 +0900

    PinBall1
    課題を提出いたします。

---
+++ Assets/Lesson5/BallController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+	private float visiblePosZ = -6.5f;
+	private GameObject gameoverText;
+	private GameObject scoreText;
+
+	public int Score = 0;
+
+	// Use this for initialization
+	void Start () {
+		gameoverText = GameObject.Find ("GameOverText");
+
+		scoreText = GameObject.Find ("Score");
+
+		Score = 0;
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.z < visiblePosZ) {
+			gameoverText.GetComponent<Text>().text = "Game Over";
+		}
+	
+	}
+
+	void OnCollisionEnter(Collision c){
+		string yourTag  = c.gameObject.tag;
+
+		if (yourTag == "SmallStarTag") {
+			Score += 10;
+		} else if (yourTag == "LargeStarTag") {
+			Score += 20;
+		} else if (yourTag == "SmallCloudTag") {
+			Score += 30;
+		} else if (yourTag == "LargeCloudTag") {
+			Score += 40;
+		}
+
+		scoreText.GetComponent<Text> ().text = "Score:"+Score;
+
+
+	}
+}
---
+++ Assets/Lesson5/FripperController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+	private HingeJoint myHingleJoynt;
+
+	private float defaultAngle = 20f;
+
+	private float flickAngle = -20f;
+
+	// Use this for initialization
+	void Start () {
+		this.myHingleJoynt = GetComponent<HingeJoint> ();
+
+		SetAngle (this.defaultAngle);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) {
+			SetAngle (this.defaultAngle);
+		}
+
+	
+	}
+
+	public void SetAngle(float angle){
+		JointSpring jointSpr = this.myHingleJoynt.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingleJoynt.spring = jointSpr;
+	}
+}
